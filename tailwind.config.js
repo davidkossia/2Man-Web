7a19beb0e087dfec92f9046ae158a4c2d25aa514
David Kossia <davidkossia@utexas.edu>
2025-04-14 15:40:15 -0700

    Initial Code Outline

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))  
